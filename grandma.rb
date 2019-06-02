e9fed2d969e1ba7eaff749ce435faa3762acd92b
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-02 21:26:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 def speak_to_grandma(phrase)
   if phrase == ("I LOVE YOU GRANDMA!").upcase
     puts "I LOVE YOU TOO PUMPKIN!"
-  end
  if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
 else 
 "NO, NOT SINCE 1938!"
+end
 end
 end
